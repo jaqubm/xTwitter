fec1e6785a6409b3500e318c5b6ea338ae0878f3
jaqubm <jaqobgm@gmail.com>
2024-05-26 22:21:06 +0200

    Created Notification routes and controller

---
+++ backend/controllers/notification.controller.js
@@ -0,0 +1,30 @@
+import Notification from "../models/notification.model.js";
+
+export const getNotifications = async (req, res) => {
+    try {
+        const userId = req.user._id;
+
+        const notifications = await Notification
+            .find({ to: userId })
+            .populate("from", "username profileImg")
+            .sort({ createdAt: -1 });
+
+        await Notification.updateMany({ to: userId }, { read: true });
+
+        res.status(200).json(notifications);
+    } catch (error) {
+        res.status(500).json({ error: "Internal Server error" });
+    }
+}
+
+export const deleteNotifications = async (req, res) => {
+    try {
+        const userId = req.user._id;
+
+        await Notification.deleteMany({ to: userId });
+
+        res.status(200).json({ message: "Notifications deleted successfully" });
+    } catch (error) {
+        res.status(500).json({ error: "Internal Server error" });
+    }
+}
---
+++ backend/routes/notification.route.js
@@ -0,0 +1,11 @@
+import express from "express";
+
+import { protectRoute } from "../middleware/protectRoute.js";
+import { getNotifications, deleteNotifications } from "../controllers/notification.controller.js";
+
+const router = express.Router();
+
+router.get("/", protectRoute, getNotifications);
+router.delete("/", protectRoute, deleteNotifications);
+
+export default router;
