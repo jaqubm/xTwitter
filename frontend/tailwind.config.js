bd514caca2bfdb401948b2ee6bf4343082052af7
jaqubm <jaqobgm@gmail.com>
2024-05-27 18:05:20 +0200

    UI Setup

---
+++ frontend/tailwind.config.js
@@ -0,0 +1,28 @@
+/** @type {import('tailwindcss').Config} */
+
+import daisyui from "daisyui";
+import daisyUIThemes from "daisyui/src/theming/themes";
+
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [daisyui],
+  daisyui: {
+		themes: [
+			"light",
+			{
+				black: {
+					...daisyUIThemes["black"],
+					primary: "rgb(29, 155, 240)",
+					secondary: "rgb(24, 24, 24)",
+				},
+			},
+		],
+	},
+}
+
