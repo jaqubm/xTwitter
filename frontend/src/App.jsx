e1792712404c86b8b0dc97fe3ca8654fb03dbf67
jaqubm <jaqobgm@gmail.com>
2024-05-27 20:27:50 +0200

    Created working SignUpPage

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,20 +1,36 @@
 import { Routes, Route } from "react-router-dom";
 
+import SideBar from "./components/common/Sidebar"
+
 import HomePage from "./pages/home/HomePage"
-import SignUpPage from "./pages/auth/signup/SignUpPage"
 import LoginPage from "./pages/auth/login/LoginPage"
+import SignUpPage from "./pages/auth/signup/SignUpPage"
+import NotificationPage from "./pages/notification/NotificationPage"
+import ProfilePage from "./pages/profile/ProfilePage"
+
+import RightPanel from "./components/common/RightPanel"
+
+import { Toaster } from "react-hot-toast"
 
 function App() {
   return (
-    <>
-      <div className='flex max-w-6xl mx-auto'>
-        <Routes>
-          <Route path='/' element={<HomePage />} />
-          <Route path='/signup' element={<SignUpPage />} />
-          <Route path='/login' element={<LoginPage />} />
-        </Routes>
-      </div>
-    </>
+    <div className='flex max-w-6xl mx-auto'>
+
+      <SideBar />
+
+      <Routes>
+        <Route path='/' element={<HomePage />} />
+        <Route path='/login' element={<LoginPage />} />
+        <Route path='/signup' element={<SignUpPage />} />
+        <Route path='/notifications' element={<NotificationPage />} />
+        <Route path='/profile/:username' element={<ProfilePage />} />
+      </Routes>
+
+      <RightPanel />
+
+      <Toaster />
+
+    </div>
   );
 }
 
